937409956073f5e3adc5e64e53f2c90c947463cf
ummaromana sama <ummaromanasama@ummaromanas-macbook-pro.local>
2018-09-04 11:36:30 -0400

    commit

--- HelloApp/ViewController.swift
+++ HelloApp/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var TextLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,5 +23,9 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func DidTapButton(_ sender: Any) {
+        print("hello")
+        TextLabel.textColor = UIColor.purple
+    }
 }
 
